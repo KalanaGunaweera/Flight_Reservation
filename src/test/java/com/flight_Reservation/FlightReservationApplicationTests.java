5b6ce0657a0491d78843031f3f33943d619b51ae
Kalana Gunaweera <kalanagunaweera94@gmail.com>
2023-11-16 21:42:43 +0530

    Initial commit

---
+++ src/test/java/com/flight_Reservation/FlightReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight_Reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
